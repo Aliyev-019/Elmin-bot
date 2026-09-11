84f2f8eab4de7d38e57ccaa759940868f667d535
Aliyev-019 <166536484+Aliyev-019@users.noreply.github.com>
2026-09-11 20:34:08 +0400

    Change AI model from mixtral to llama

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ client = Groq(api_key=GROQ_API_KEY)
 # Groq AI cavabı
 def get_ai_response(text):
     completion = client.chat.completions.create(
-        model="mixtral-8x7b-32768",
+        model="llama-3.2-3b-preview",
         messages=[{"role": "user", "content": text}]
     )
     return completion.choices[0].message.content
